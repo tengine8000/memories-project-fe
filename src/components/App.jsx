d785b4b8beb7a7f9826e391985700bd9f9ec4fc2
tengine8000 <torty.emmanuel@gmail.com>
2021-08-15 13:49:21 +0100

    Enable form submission and posts retrieval from server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from '../images/memories.jpg';
+import Form from './Form/Form';
+import Posts from './Posts/Posts';
+import { useDispatch } from 'react-redux';
+import { getPosts } from '../actions/posts'
 
+import useStyles from './styles';
+    
 const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return (
-        <div>
-            <h1> Hello Mernie</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position='static' color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">
+                        Memories
+                </Typography>
+                <img className={classes.image} src={memories} alt="Memories" height="60" />
+            </AppBar>
+            <Grow in >
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+
+            </Grow>
+        </Container>
     )
 }
 

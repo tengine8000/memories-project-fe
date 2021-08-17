7ab6c70cee19c9d123d8268206502fae156af9b8
tengine8000 <torty.emmanuel@gmail.com>
2021-08-17 16:19:57 +0100

    Update BE URL

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -4,15 +4,15 @@ import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionType
 export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
-      return action.payload.sort((a, b) => a - b);
+      return action.payload;
     case CREATE:
-      return [...posts, action.payload].sort((a, b) => a - b);
+      return [...posts, action.payload];
     case UPDATE:
     case LIKE:
-      return posts.map((post) => post._id === action.payload._id ? action.payload : post).sort((a, b) => a - b);
+      return posts.map((post) => post._id === action.payload._id ? action.payload : post);
     case DELETE:
-      return posts.filter((post) => (post._id !== action.payload._id)).sort((a, b) => a - b);
+      return posts.filter((post) => (post._id !== action.payload._id));
     default:
-      return posts.sort((a, b) => a - b);
+      return posts;
   }
 };

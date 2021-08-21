6a7321eb0b6dc70eed0cc0ddfc61f8f697b267fe
tengine8000 <torty.emmanuel@gmail.com>
2021-08-21 13:52:13 +0100

    Enable Gmail sign in

---
+++ src/actions/auth.js
@@ -0,0 +1,10 @@
+// import { AUTH } from "../constants/actionTypes";
+
+// export const getPosts = () => async (dispatch) => {
+//     try {
+//         const { data } = await api.fetchPosts()
+//         dispatch({ type: FETCH_ALL, payload: data });
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import memories from '../images/memories.jpg';
-import Form from './Form/Form';
-import Posts from './Posts/Posts';
-import { useDispatch } from 'react-redux';
-import { getPosts } from '../actions/posts'
-
-import useStyles from './styles';
+import React  from 'react';
+import { Container} from '@material-ui/core';
+import { BrowserRouter,Switch, Route } from 'react-router-dom'; 
+import Navbar from './Navbar/Navbar';
+import Home from './Home/Home';
+import Auth from './Auth/Auth';
     
 const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
     return (
-        <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position='static' color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">
-                        Memories
-                </Typography>
-                <img className={classes.image} src={memories} alt="Memories" height="60" />
-            </AppBar>
-            <Grow in >
-                <Container>
-                    <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts  setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-
-            </Grow>
-        </Container>
+        <BrowserRouter>
+            <Container maxWidth="lg">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/auth" exact component={Auth} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
     )
 }
 
